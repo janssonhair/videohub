4fe390c1483bc299aad5cfe0d7eb846e834fc973
janssonhair <janssonhair@gmail.com>
2026-07-28 16:34:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const SUPABASE_URL = "https://dxvhibiteowkjfiadeu.supabase.co";
-
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dmhoaWJpdGVvd2tqZmlhZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMDc0OTEsImV4cCI6MjEwMDc4MzQ5MX0.LOHFJW5L78bOnjyiim9ZbKa9edvShpuO7ZpG1Oy2O48";
+let videos = [];
 
 
-async function loadVideos(){
+fetch("videos.json")
+.then(res => res.json())
+.then(data => {
 
-const response = await fetch(
-`${SUPABASE_URL}/rest/v1/videos`,
-{
-headers:{
-apikey: SUPABASE_KEY,
-Authorization:`Bearer ${SUPABASE_KEY}`
-}
-}
-);
+videos = data;
+
+showVideos(videos);
+
+});
 
 
-const videos = await response.json();
 
+function showVideos(list){
 
-let box = document.getElementById("videos");
+let box=document.getElementById("videos");
 
 box.innerHTML="";
 
 
-videos.forEach(v=>{
+list.forEach(v=>{
+
 
 box.innerHTML += `
 
@@ -36,18 +33,20 @@ box.innerHTML += `
 
 <p>${v.category}</p>
 
+
 <a href="${v.url}" target="_blank">
+
 ▶ 播放视频
+
 </a>
+
 
 </div>
 
 `;
 
+
 });
 
 
 }
-
-
-loadVideos();
