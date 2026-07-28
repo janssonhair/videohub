11a170cac0560d60e8b1f2a1525cac891d5e89ba
janssonhair <janssonhair@gmail.com>
2026-07-28 11:29:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+﻿let data=[];
+
+
+fetch("videos.json")
+.then(res=>res.json())
+.then(videos=>{
+
+data=videos;
+show(videos);
+
+});
+
+
+function show(list){
+
+let box=document.getElementById("videos");
+
+box.innerHTML="";
+
+
+list.forEach(v=>{
+
+box.innerHTML+=`
+
+<div class="card">
+
+<img src="${v.cover}">
+
+<h3>${v.title}</h3>
+
+<p>${v.category}</p>
+
+
+<a href="${v.url}" target="_blank">
+
+▶ 播放视频
+
+</a>
+
+</div>
+
+`;
+
+});
+
+}
+
+
+
+document
+.getElementById("search")
+.onkeyup=function(){
+
+let key=this.value;
+
+
+let result=data.filter(v=>
+
+v.title.includes(key)
+
+);
+
+
+show(result);
+
+
+}
