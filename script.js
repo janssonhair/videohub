0eca83ab5819d557d0ab3555be57082864d89952
janssonhair <janssonhair@gmail.com>
2026-07-28 17:47:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+let allVideos = [];
+
+
+// 加载视频
+
 async function loadVideos(){
 
     const response = await fetch("videos.json");
 
-    const videos = await response.json();
+    allVideos = await response.json();
+
+    showVideos(allVideos);
+
+}
+
+
+
+// 显示视频
+
+function showVideos(videos){
 
     const box = document.getElementById("videos");
 
     box.innerHTML = "";
 
 
-    videos.forEach(v => {
+    videos.forEach(v=>{
+
 
         box.innerHTML += `
 
         <div class="card">
+
 
             <img class="cover" src="${v.cover}">
 
@@ -30,33 +47,104 @@ async function loadVideos(){
                 </div>
 
 
+
                 <h3>${v.title}</h3>
 
 
                 <p class="meta">
-                    ${v.views} · ${v.time}
+                ${v.views} · ${v.time}
                 </p>
 
 
                 <p>
-                    ${v.description}
+                ${v.description}
                 </p>
 
 
                 <a href="${v.url}" target="_blank">
-                    ▶ 播放视频
+                ▶ 播放视频
                 </a>
 
 
             </div>
 
+
         </div>
 
         `;
 
+
     });
 
+
 }
+
+
+
+// 搜索功能
+
+document.getElementById("search")
+.addEventListener("input",function(){
+
+
+    let keyword=this.value.toLowerCase();
+
+
+    let result=allVideos.filter(v=>{
+
+
+        return (
+
+        v.title.toLowerCase().includes(keyword)
+
+        ||
+
+        v.category.toLowerCase().includes(keyword)
+
+        ||
+
+        v.channel.toLowerCase().includes(keyword)
+
+        );
+
+
+    });
+
+
+    showVideos(result);
+
+
+});
+
+
+
+
+// 分类筛选
+
+function filterCategory(category){
+
+
+    if(category==="全部"){
+
+        showVideos(allVideos);
+
+        return;
+
+    }
+
+
+    let result=allVideos.filter(v=>
+
+        v.category===category
+
+    );
+
+
+    showVideos(result);
+
+
+}
+
 
 
 loadVideos();
